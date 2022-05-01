8c6957bb4e29e2bd298dbb3888caab4c9e223387
dawid <dawidtomczynski@gmail.com>
2022-05-01 16:10:53 +0200

    zrobione, bez zapętlenia 'nie oszukuj'

--- guess_the_number_beat_the_computer.py
+++ guess_the_number_beat_the_computer.py
@@ -1,21 +1,25 @@
 print("Pomyśl liczbę od 0 do 1000, a ja ją zgadnę w max. 10 próbach")
 
-min = 0
-max = 1000
+min_num = 0
+max_num = 1000
 ans = ""
+ans_list = []
 while not ans == "zgadłeś":
-    # for i in range(10):
-        guess = int((max-min) / 2) + min
+    while not len(ans_list) == 10:
+        guess = int((max_num-min_num) / 2) + min_num
         print("Zgaduję:", guess)
         ans = input("")
         if ans == "za mało":
             min = guess
+            ans_list.append(ans)
         elif ans == "za dużo":
             max = guess
+            ans_list.append(ans)
         elif ans == "zgadłeś":
             print("Wygrałem!")
             break
         else:
             print("Podaj: za mało, za dużo lub zgadłeś.")
-    # else:
-    #     print("Nie oszukuj!")
+    else:
+        print("Nie oszukuj!")
+        break
